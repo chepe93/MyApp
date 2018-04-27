352b992756cb2ad3de436d545e05cd7f746700db
emmy88 <37887806+emmy88@users.noreply.github.com>
2018-04-27 14:23:11 -0500

    pru

--- app/src/main/java/com/app/checklist/Croquis.java
+++ app/src/main/java/com/app/checklist/Croquis.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -20,11 +22,13 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.Toolbar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,8 +37,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+//import ulr.co.setab.phothoview.PhotoViewAtacher;
+
 import me.panavtec.drawableview.DrawableView;
 import me.panavtec.drawableview.DrawableViewConfig;
+
+
 
 public class Croquis extends Fragment implements View.OnClickListener{
     public View main_view;
@@ -47,10 +55,24 @@ public class Croquis extends Fragment implements View.OnClickListener{
     Bitmap croquis;
     String pruebaGithub="hola errores";
 
+    ImageView rectangle,circle;
+    Toolbar toolbar;
+    float scalediff;
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    private float oldDist = 1f;
+    private float d = 0f;
+    private float newRot = 0f;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO Auto-generated method stub
+
+        super.onCreate(savedInstanceState);
+
 
 
         main_view=inflater.inflate(R.layout.fragment_croquis, container, false);
@@ -93,8 +115,6 @@ public class Croquis extends Fragment implements View.OnClickListener{
 
         }
 
-
-
         getActivity().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         return main_view;
     }
@@ -105,34 +125,105 @@ public class Croquis extends Fragment implements View.OnClickListener{
     }
 
     public class ChoiceTouchListener implements View.OnTouchListener{
+        RelativeLayout.LayoutParams parms;
+        int startwidth;
+        int startheight;
+        float dx = 0, dy = 0, x = 0, y = 0;
+        float angle = 0;
+
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            final int X = (int) motionEvent.getRawX();
-            final int Y = (int) motionEvent.getRawY();
-            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+        public boolean onTouch(View v, MotionEvent event) {
+            final ImageView view = (ImageView) v;
+
+            ((BitmapDrawable) view.getDrawable()).setAntiAlias(true);
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
                 case MotionEvent.ACTION_DOWN:
-                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                    _xDelta = X - lParams.leftMargin;
-                    _yDelta = Y - lParams.topMargin;
+
+                    parms = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    startwidth = parms.width;
+                    startheight = parms.height;
+                    dx = event.getRawX() - parms.leftMargin;
+                    dy = event.getRawY() - parms.topMargin;
+                    mode = DRAG;
+                    break;
+
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    oldDist = spacing(event);
+                    if (oldDist > 10f) {
+                        mode = ZOOM;
+                    }
+
+                    d = rotation(event);
+
                     break;
                 case MotionEvent.ACTION_UP:
+
                     break;
-                case MotionEvent.ACTION_POINTER_DOWN:
-                    break;
+
                 case MotionEvent.ACTION_POINTER_UP:
+                    mode = NONE;
+
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-                            .getLayoutParams();
-                    layoutParams.leftMargin = X - _xDelta;
-                    layoutParams.topMargin = Y - _yDelta;
-                    layoutParams.rightMargin = -250;
-                    layoutParams.bottomMargin = -250;
-                    view.setLayoutParams(layoutParams);
+                    if (mode == DRAG) {
+
+                        x = event.getRawX();
+                        y = event.getRawY();
+
+                        parms.leftMargin = (int) (x - dx);
+                        parms.topMargin = (int) (y - dy);
+
+                        parms.rightMargin = 0;
+                        parms.bottomMargin = 0;
+                        parms.rightMargin = parms.leftMargin + (5 * parms.width);
+                        parms.bottomMargin = parms.topMargin + (10 * parms.height);
+
+                        view.setLayoutParams(parms);
+
+                    } else if (mode == ZOOM) {
+
+                        if (event.getPointerCount() == 2) {
+
+                            newRot = rotation(event);
+                            float r = newRot - d;
+                            angle = r;
+
+                            x = event.getRawX();
+                            y = event.getRawY();
+
+                            float newDist = spacing(event);
+                            if (newDist > 10f) {
+                                float scale = newDist / oldDist * view.getScaleX();
+                                if (scale > 0.6) {
+                                    scalediff = scale;
+                                    view.setScaleX(scale);
+                                    view.setScaleY(scale);
+
+                                }
+                            }
+
+                            view.animate().rotationBy(angle).setDuration(0).setInterpolator(new LinearInterpolator()).start();
+
+                            x = event.getRawX();
+                            y = event.getRawY();
+
+                            parms.leftMargin = (int) ((x - dx) + scalediff);
+                            parms.topMargin = (int) ((y - dy) + scalediff);
+
+                            parms.rightMargin = 0;
+                            parms.bottomMargin = 0;
+                            parms.rightMargin = parms.leftMargin + (5 * parms.width);
+                            parms.bottomMargin = parms.topMargin + (10 * parms.height);
+
+                            view.setLayoutParams(parms);
+
+
+                        }
+                    }
                     break;
             }
-            view.invalidate();
             return true;
+
         }
     }
 
@@ -161,6 +252,11 @@ public class Croquis extends Fragment implements View.OnClickListener{
             }
             return true;
         }
+    }
+
+    @Override
+    public boolean onLongClick(View v){
+        
     }
 
     @Override
@@ -239,6 +335,8 @@ public class Croquis extends Fragment implements View.OnClickListener{
                 view.addView(option);
                 break;
 
+
+
             case R.id.btnCirculo:
                 option = new ImageView(getActivity());
                 option.setImageResource(R.drawable.circle);
@@ -251,5 +349,21 @@ public class Croquis extends Fragment implements View.OnClickListener{
         }
     }
 
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
 
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
 }
+
+
+
+
+
